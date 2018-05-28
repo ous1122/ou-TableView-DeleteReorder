fefbedbba4725248b3e6852069f81ade49129aec
D7703_24 <d7703_24@d7703-24ui-imac.local>
2018-05-28 15:37:54 +0900

    123

--- Custom TableView Cell/FoodStoreTableViewController.swift
+++ Custom TableView Cell/FoodStoreTableViewController.swift
@@ -28,7 +28,8 @@ class FoodStoreTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.title = " DIT 철가방"
     }
-
+    
+    
 
     // MARK: - Table view data source
 
@@ -77,15 +78,17 @@ class FoodStoreTableViewController: UITableViewController {
         optionMenu.addAction(callAction)
         optionMenu.addAction(cancelAction)
         present(optionMenu, animated: true, completion: nil)
-    /*
+    }
+        
+        
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -95,14 +98,21 @@ class FoodStoreTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
-    /*
+
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        let tmp1 = foodStoreNames[to.row]
+        foodStoreNames[to.row] = foodStoreNames[fromIndexPath.row]
+        foodStoreNames[fromIndexPath.row] = tmp1
+        
+        let tmp2 = foodStoreImages[to.row]
+        foodStoreImages[to.row] = foodStoreImages[fromIndexPath.row]
+        foodStoreImages[fromIndexPath.row] = tmp2
+        
+        tableView.reloadData()
     }
-    */
 
     /*
     // Override to support conditional rearranging of the table view.
@@ -119,8 +129,8 @@ class FoodStoreTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.destination == "showDetail"
+ 
     }
-    */
-
-}
+*/
 }
